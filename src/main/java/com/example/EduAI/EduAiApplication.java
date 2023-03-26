613924a13e94a05ef2e9454dcc3be3d1dabe352f
thaveesi <thaveesi@usc.edu>
2023-03-26 00:09:47 -0700

    initial commit

---
+++ src/main/java/com/example/EduAI/EduAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.EduAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduAiApplication.class, args);
+	}
+
+}
