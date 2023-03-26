f9e93bee42090642eff60d421e4408c2832e1c3d
thaveesi <thaveesi@usc.edu>
2023-03-26 04:17:44 -0700

    added neccesary skeleton for gpt3

--- src/main/java/com/example/EduAI/service/GPTService.java
+++ src/main/java/com/example/EduAI/service/GPTService.java
@@ -1,4 +1,20 @@
 package com.example.EduAI.service;
 
+import com.example.EduAI.dto.ResourceDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class GPTService {
+
+    // Add properties for GPT-3 API credentials and settings
+
+    public List<ResourceDto> generateResourceSuggestions(String topicName) {
+        // Add logic for making a request to the GPT-3 API using the topicName as a prompt
+        // Parse the API response into a list of ResourceDto objects
+        // Return the list of ResourceDto objects
+    }
+
+    // Add any additional methods or configurations required for GPTService
 }
