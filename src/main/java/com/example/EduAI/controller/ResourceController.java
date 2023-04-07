dc3188dd6299b4a5a030fe2eda9d3a29201b71cd
thaveesi <thaveesi@usc.edu>
2023-04-07 01:36:52 -0700

    finished code for user based classes

--- src/main/java/com/example/EduAI/controller/ResourceController.java
+++ src/main/java/com/example/EduAI/controller/ResourceController.java
@@ -15,34 +15,34 @@ public class ResourceController {
 
     @Autowired
     private ResourceService resourceService;
-
+//
     @Autowired
     private GPTService gptService;
-
-    @PostMapping
-    public ResponseEntity<ResourceDto> createResource(@RequestBody ResourceDto resourceDto) {
-        // Add logic for creating a resource
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<ResourceDto> updateResource(@PathVariable Long id, @RequestBody ResourceDto resourceDto) {
-        // Add logic for updating a resource
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteResource(@PathVariable Long id) {
-        // Add logic for deleting a resource
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<ResourceDto> getResource(@PathVariable Long id) {
-        // Add logic for retrieving a resource
-    }
-
-    @GetMapping
-    public ResponseEntity<List<ResourceDto>> getAllResources() {
-        // Add logic for retrieving all resources
-    }
+//
+//    @PostMapping
+//    public ResponseEntity<ResourceDto> createResource(@RequestBody ResourceDto resourceDto) {
+//        // Add logic for creating a resource
+//    }
+//
+//    @PutMapping("/{id}")
+//    public ResponseEntity<ResourceDto> updateResource(@PathVariable Long id, @RequestBody ResourceDto resourceDto) {
+//        // Add logic for updating a resource
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<Void> deleteResource(@PathVariable Long id) {
+//        // Add logic for deleting a resource
+//    }
+//
+//    @GetMapping("/{id}")
+//    public ResponseEntity<ResourceDto> getResource(@PathVariable Long id) {
+//        // Add logic for retrieving a resource
+//    }
+//
+//    @GetMapping
+//    public ResponseEntity<List<ResourceDto>> getAllResources() {
+//        // Add logic for retrieving all resources
+//    }
 
     @PostMapping("/generate")
     public ResponseEntity<List<ResourceDto>> generateResources(@RequestBody String topicName) {
--- src/main/java/com/example/EduAI/dto/ResourceDto.java
+++ src/main/java/com/example/EduAI/dto/ResourceDto.java
@@ -1,10 +1,16 @@
 package com.example.EduAI.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ResourceDto {
 
     private Long id;
     private String name;
     private String url;
+    private String type;
 
     // Add getters, setters, and constructors.
 }
